42d7b97154ac8503324f838040e25be693ebe2a1
techstackspace <info@techstackspace.com>
2024-08-14 16:30:55 +0000

    feat: implement product interface

---
+++ src/models/product/interface.ts
@@ -0,0 +1,21 @@
+import { IComment } from '../comment/interface';
+import { IUser } from '../user/interface';
+
+interface IProduct {
+  name: string;
+  description: string;
+  price: number;
+  image?: string[];
+  video?: string[];
+  pdf?: string[];
+  category: string;
+  rating?: number;
+  reviews?: string;
+  tags: string[];
+  brand: string;
+  stock: number;
+  likes: IUser;
+  comments: IComment;
+}
+
+export type { IProduct };
