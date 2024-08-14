a38cf4b6456a7d79edcb9e040dc2245ea54e564d
techstackspace <info@techstackspace.com>
2024-08-14 00:17:37 +0000

    feat: add product routes and link to controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createProduct, getAllProducts } from '../controllers';
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProductPartial,
+} from '../controllers';
 
 const router = express.Router();
 
 router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById);
 router.post('/products', createProduct);
+router.patch('/products/:id', updateProductPartial);
+router.delete('/products/:id', deleteProduct);
 
 export default router;
