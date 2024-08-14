36408b285c3c8c6a2451eee558b87917c77afb0d
techstackspace <info@techstackspace.com>
2024-08-14 16:25:26 +0000

    feat: add Product schema with references to User and Comment schemas

---
+++ src/models/product/index.ts
@@ -0,0 +1,25 @@
+import { Document, model, Schema } from 'mongoose';
+import { IProduct } from './interface';
+
+const ProductSchema = new Schema<IProduct & Document>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: [String], default: [] },
+    video: { type: [String], default: [] },
+    pdf: { type: [String], default: [] },
+    category: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    reviews: { type: String, default: '' },
+    tags: { type: [String], required: true },
+    brand: { type: String, required: true },
+    stock: { type: Number, required: true },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment', default: [] }],
+  },
+  { timestamps: true }
+);
+
+const Product = model<IProduct & Document>('Product', ProductSchema);
+export default Product;
