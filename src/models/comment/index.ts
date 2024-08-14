ec0b7e99e98c64fb4671e2d078d2314772c74a21
techstackspace <info@techstackspace.com>
2024-08-14 20:11:27 +0000

    feat(comment-schema): add custom version key

--- src/models/comment/index.ts
+++ src/models/comment/index.ts
@@ -6,8 +6,9 @@ const CommentSchema = new Schema<IComment & Document>(
     content: { type: String, required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    version: { type: String, default: '1.0.0' },
   },
-  { timestamps: true }
+  { timestamps: true, versionKey: 'version' }
 );
 
 const Comment = model<IComment & Document>('Comment', CommentSchema);
