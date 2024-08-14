f81320a66c81540283b7fc97e82353597cd77add
techstackspace <info@techstackspace.com>
2024-08-14 17:15:06 +0000

    feat(user): implement registration controller with no password hashing

---
+++ src/controllers/user/auth/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import User from '../../../models/user';
+
+const register = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Missing required field' });
+  }
+
+  try {
+    const user = new User();
+    const savedUser = await user.save();
+    return res.status(201).json({ user: savedUser });
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      return res.status(500).json({ error: 'Unknown error occured' });
+    }
+  }
+};
+
+export default register;
