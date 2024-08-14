e8726587946a356f920954b5b6d9b3cc2f6ebf66
techstackspace <info@techstackspace.com>
2024-08-14 17:56:08 +0000

    feat: mount the /api route to the user registration endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { json } from 'express';
 import { log } from './config/debugger';
 import connectionToDB from './config/database';
-import productRoutes from './routes';
+import productRoutes from './routes/product';
+import userRoutes from './routes/auth/register';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ connectionToDB();
 
 // Routes
 app.use('/api', productRoutes);
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
   log(`Server is running on http://localhost:${port}`);
