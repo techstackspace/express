4094ba1a0956a34f9952103d9c983bad2cce8654
techstackspace <info@techstackspace.com>
2024-08-14 18:57:18 +0000

    feat: add user deletion route with :id parameter

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,6 +4,6 @@ import { registerUser, deleteUser } from '../../controllers/user/auth';
 const router = Router();
 
 router.post('/user/register', registerUser);
-router.delete('/user', deleteUser);
+router.delete('/user/:id', deleteUser);
 
 export default router;
