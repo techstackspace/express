11f913aa9e3b94c1a033ed32a656f59e699f1e58
techstackspace <info@techstackspace.com>
2024-08-14 15:05:47 +0000

    feat: provide more properties to schema like image, video, and pdf
    - change rating type from srtring to number
    - add more properties to schemawhich are image, video, and pdf

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,21 +1,25 @@
 import { model, Schema } from 'mongoose';
+import { IProduct } from './interface';
 
-const ProductSchema = new Schema(
+const ProductSchema = new Schema<IProduct>(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
+    image: { type: [String], default: [] },
+    video: { type: [String], default: [] },
+    pdf: { type: [String], default: [] },
     category: { type: String, required: true },
-    rating: String,
-    reviews: String,
+    rating: { type: Number, default: 0 },
+    reviews: { type: String, default: '' },
     tags: { type: [String], required: true },
     brand: { type: String, required: true },
     stock: { type: Number, required: true },
-    likes: { type: [String], required: true },
-    comments: { type: [String], required: true },
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
   },
   { timestamps: true }
 );
 
-const Product = model('Product', ProductSchema);
+const Product = model<IProduct>('Product', ProductSchema);
 export default Product;
