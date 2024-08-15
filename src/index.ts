0b2027ef324a91842c968ae16371cb8281341597
techstackspace <info@techstackspace.com>
2024-08-15 00:41:24 +0000

    feat: mount /api route to comment endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express, { json } from 'express';
 import { log } from './config/debugger';
 import connectionToDB from './config/database';
 import productRoutes from './routes/product';
-import userRoutes from './routes/auth';
+import userRoutes from './routes/user';
+import commentRoutes from './routes/comment';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ connectionToDB();
 // Routes
 app.use('/api', productRoutes);
 app.use('/api', userRoutes);
+app.use('/api', commentRoutes);
 
 app.listen(port, () => {
   log(`Server is running on http://localhost:${port}`);
