6d5e4e8c3ea7f74bcf0a79da62a46c4fed78e978
techstackspace <info@techstackspace.com>
2024-08-15 00:39:02 +0000

    feat: extend Comment Interface from Document

--- src/models/comment/index.ts
+++ src/models/comment/index.ts
@@ -1,4 +1,4 @@
-import { model, Schema } from 'mongoose';
+import { Document, model, Schema } from 'mongoose';
 import { IComment } from './interface';
 
 const CommentSchema = new Schema<IComment & Document>(
