5eb9b82856b282720f4addb8cc32de1d74937d09
techstackmedia <techstackmedia@gmail.com>
2024-08-19 20:04:16 +0000

    feat: add address schema such as two non-default addresses and one default address

---
+++ src/models/address/index.ts
@@ -0,0 +1,16 @@
+import { Document, model, Schema } from 'mongoose';
+import { IAddress } from './interface';
+
+const AddressSchema = new Schema<IAddress & Document>({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  addressLine1: { type: String, required: true },
+  addressLine2: { type: String },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  country: { type: String, required: true },
+  isDefault: { type: Boolean, default: false },
+});
+
+const Address = model<IAddress & Document>('Address', AddressSchema);
+export default Address;
