37e905202662f90223d38e43a130e5b900f1c5fc
techstackmedia <techstackmedia@gmail.com>
2024-08-19 23:41:38 +0000

    feat: include user in request body to retrieve address

--- src/controllers/address/index.ts
+++ src/controllers/address/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Address from '../../models/address';
+import User from '../../models/user';
 
 const createAddress = async (req: Request, res: Response) => {
   try {
@@ -85,9 +86,10 @@ const updateAddress = async (req: Request, res: Response) => {
   }
 };
 
-const getUserAddresses = async (_req: Request, res: Response) => {
+const getUserAddresses = async (req: Request, res: Response) => {
+  const user = await User.findById(req.body.user);
   try {
-    const addresses = await Address.find();
+    const addresses = await Address.find({ user });
     res.status(200).json(addresses);
   } catch (error) {
     if (error instanceof Error) {
