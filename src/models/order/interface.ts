c12247eadfc2888c493a09a16298c0cd1b24fae1
techstackmedia <techstackmedia@gmail.com>
2024-08-19 16:47:06 +0000

    feat(types): define order product interfaces

---
+++ src/models/order/interface.ts
@@ -0,0 +1,22 @@
+import { IProduct } from '../product/interface';
+import { IUser } from '../user/interface';
+
+interface IOrderProduct {
+  product: IProduct;
+  quantity: number;
+  price: number;
+}
+
+interface IOrder extends Document {
+  user: IUser;
+  products: IOrderProduct[];
+  totalAmount: number;
+  orderStatus: string;
+  shippingAddress: string;
+  paymentMethod: string;
+  isPaid: boolean;
+  paidAt?: Date;
+  deliveredAt?: Date;
+}
+
+export type { IOrderProduct, IOrder };
