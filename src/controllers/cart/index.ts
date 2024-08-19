8665c06c04a33f51e9188928e95706c8825f390a
techstackmedia <techstackmedia@gmail.com>
2024-08-19 20:00:23 +0000

    feat: add user id as part of the request in the json body

--- src/controllers/cart/index.ts
+++ src/controllers/cart/index.ts
@@ -2,9 +2,11 @@ import { Request, Response } from 'express';
 import { error } from '../../config/debugger';
 import Cart from '../../models/cart';
 
-const getCartItems = async (_req: Request, res: Response) => {
+const getCartItems = async (req: Request, res: Response) => {
   try {
-    const cartItems = await Cart.find().populate('product');
+    const cartItems = await Cart.find({ user: req.body.user }).populate(
+      'product'
+    );
     return res.status(200).json(cartItems);
   } catch (err) {
     if (err instanceof Error) {
@@ -17,21 +19,22 @@ const getCartItems = async (_req: Request, res: Response) => {
 };
 
 const addToCart = async (req: Request, res: Response) => {
-  const { product, quantity } = req.body;
-  if (!product || !quantity) {
+  const { product, quantity, user } = req.body;
+
+  if (!product || !quantity || !user) {
     return res
       .status(400)
-      .json({ message: 'Product and quantity are required.' });
+      .json({ message: 'User, product, and quantity are required.' });
   }
 
   try {
-    const existingCartItem = await Cart.findOne({ product });
+    const existingCartItem = await Cart.findOne({ product, user });
     if (existingCartItem) {
       existingCartItem.quantity += quantity;
       const updatedCartItem = await existingCartItem.save();
       return res.status(200).json(updatedCartItem);
     } else {
-      const cart = new Cart({ product, quantity });
+      const cart = new Cart({ product, quantity, user });
       const savedCart = await cart.save();
       return res.status(201).json(savedCart);
     }
@@ -47,9 +50,14 @@ const addToCart = async (req: Request, res: Response) => {
 
 const deleteFromCart = async (req: Request, res: Response) => {
   const { id } = req.params;
+  const { user } = req.body;
+
+  if (!user) {
+    return res.status(400).json({ message: 'User is required.' });
+  }
 
   try {
-    const cart = await Cart.findByIdAndDelete(id);
+    const cart = await Cart.findOneAndDelete({ _id: id, user });
     if (!cart) {
       return res.status(404).json({ message: 'Cart item not found.' });
     }
