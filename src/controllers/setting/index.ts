c67609ff3e2f22ade1e39cdc68ad4a6c3160b616
techstackmedia <techstackmedia@gmail.com>
2024-08-20 10:31:53 +0000

    feat: implement setting controllers

---
+++ src/controllers/setting/index.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import ProfileSettings from '../../models/setting';
+
+export const getProfileSettings = async (req: Request, res: Response) => {
+  try {
+    const settings = await ProfileSettings.findOne();
+    if (!settings) {
+      return res.status(404).json({ message: 'Settings not found' });
+    }
+    res.json(settings);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+export const createOrUpdateProfileSettings = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { theme, profileVisibility, dataSharing } = req.body;
+
+    let settings = await ProfileSettings.findOne();
+    if (settings) {
+      settings.theme = theme || settings.theme;
+      settings.profileVisibility =
+        profileVisibility || settings.profileVisibility;
+      settings.dataSharing =
+        dataSharing !== undefined ? dataSharing : settings.dataSharing;
+      await settings.save();
+      return res.json({ message: 'Settings updated successfully', settings });
+    } else {
+      settings = new ProfileSettings({ theme, profileVisibility, dataSharing });
+      await settings.save();
+      return res
+        .status(201)
+        .json({ message: 'Settings created successfully', settings });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+export const deleteProfileSettings = async (req: Request, res: Response) => {
+  try {
+    const settings = await ProfileSettings.findOneAndDelete();
+    if (!settings) {
+      return res.status(404).json({ message: 'Settings not found' });
+    }
+    res.json({ message: 'Settings deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
