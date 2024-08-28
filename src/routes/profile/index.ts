a1373b30d0a52d5f8eed412a3c5c680c565a9617
techstackmedia <techstackmedia@gmail.com>
2024-08-28 23:38:15 +0000

    chore: prettier effect

--- src/routes/profile/index.ts
+++ src/routes/profile/index.ts
@@ -7,13 +7,29 @@ import {
   createProfile,
 } from '../../controllers/profile';
 import { authenticateToken } from '../../middleware';
+import upload from '../../config/multer';
 
 const router = express.Router();
 
 router.get('/profile', getProfile);
-router.put('/profile', authenticateToken, updateProfile);
+router.put(
+  '/profile',
+  authenticateToken,
+  upload.single('profilePicture'),
+  updateProfile
+);
 router.delete('/profile', authenticateToken, deleteProfile);
-router.patch('/profile', authenticateToken, patchProfile);
-router.post('/profile', authenticateToken, createProfile);
+router.patch(
+  '/profile',
+  authenticateToken,
+  upload.single('profilePicture'),
+  patchProfile
+);
+router.post(
+  '/profile',
+  authenticateToken,
+  upload.single('profilePicture'),
+  createProfile
+);
 
 export default router;
