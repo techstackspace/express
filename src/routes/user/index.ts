98ea7aaeccc59bc0bec3ce6ea112062a4f18edce
techstackmedia <techstackmedia@gmail.com>
2024-08-28 20:02:30 +0000

    chore: reorder defined routes

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -12,10 +12,10 @@ import { authenticateToken } from '../../middleware';
 const router = Router();
 
 router.get('/users', getAllUsers);
-router.get('/users/:id', getUserById);
 router.post('/users/register', createUser);
 router.post('/users/login', loginUser);
 router.post('/users/logout', logoutUser);
+router.get('/users/:id', getUserById);
 router.delete('/users/:id', authenticateToken, deleteUser);
 
 export default router;
