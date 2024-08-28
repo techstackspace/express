e6d3e470927644d03a62fcd0e0def434b9aa4559
techstackmedia <techstackmedia@gmail.com>
2024-08-28 16:08:16 +0000

    feat: enforce rule to add validation in order to prevent spaces in username

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -4,13 +4,24 @@ import { IUser } from './interface';
 const UserSchema = new Schema<IUser & Document>(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
+    username: { 
+      type: String, 
+      required: true, 
+      unique: true, 
+      validate: {
+        validator: function(v: string) {
+          return /^[\w]+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid username!`
+      }
+    },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     version: { type: Number, default: 1 },
   },
   { timestamps: true }
 );
+
 
 const User = model<IUser & Document>('User', UserSchema);
 export default User;
