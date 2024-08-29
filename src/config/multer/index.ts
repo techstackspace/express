645ba0adf581faf1b57b468816cb8af853c76b5e
techstackmedia <techstackmedia@gmail.com>
2024-08-29 00:09:59 +0000

    feat: dynamically set image format with switch cases

--- src/config/multer/index.ts
+++ src/config/multer/index.ts
@@ -2,25 +2,34 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import cloudinary from '../cloudinary';
 
-interface ITransformation {
-  width: number;
-  height: number;
-  crop: string;
+interface CloudinaryParams {
+  folder: string;
+  format: () => Promise<string>;
+  transformation?: { width: number; height: number; crop: string }[];
 }
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+const getFormat = async (_req: Express.Request, file: Express.Multer.File): Promise<string> => {
+  const mimeType = file.mimetype;
+  switch (mimeType) {
+    case 'image/jpeg':
+    case 'image/jpg':
+      return 'jpg';
+    case 'image/png':
+      return 'png';
+    case 'image/avif':
+      return 'avif';
+    default:
+      throw new Error('Unsupported image format');
+  }
+};
+
+const imageStorage = new CloudinaryStorage({
+  cloudinary,
   params: {
-    folder: 'profile_pictures',
-    allowed_formats: ['jpg', 'png'],
+    folder: 'profiles',
+    format: getFormat,
     transformation: [{ width: 500, height: 500, crop: 'limit' }],
-  } as {
-    folder: string;
-    allowed_formats: string[];
-    transformation: ITransformation[];
-  },
+  } as CloudinaryParams,
 });
 
-const upload = multer({ storage: storage });
-
-export default upload;
+export const uploadImage = multer({ storage: imageStorage });
