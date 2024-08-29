26dcefc3030ed158e9d93751d3bbacf4172ce3d9
techstackmedia <techstackmedia@gmail.com>
2024-08-29 19:42:44 +0000

    chore: prettier effect

--- src/config/multer/product/index.ts
+++ src/config/multer/product/index.ts
@@ -4,25 +4,25 @@ import cloudinary from '../../cloudinary';
 import { Request } from 'express';
 
 interface CloudinaryParams {
-    folder: string;
-    resource_type: string;
-    format: () => Promise<string>;
-    public_id: (_req: Request, file: Express.Multer.File) => string;
-  }
+  folder: string;
+  resource_type: string;
+  format: () => Promise<string>;
+  public_id: (_req: Request, file: Express.Multer.File) => string;
+}
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-      folder: 'products',
-      resource_type: 'auto',
-      format: async (_req: Request, file: { mimetype: string; }) => {
-          if (file.mimetype.startsWith('image/')) return 'jpg';
-          if (file.mimetype.startsWith('video/')) return 'mp4';
-          return 'bin';
-      },
-      public_id: (_req: Request, file: Express.Multer.File) => {
-          return `${Date.now()}-${file.originalname}`;
-      },
+    folder: 'products',
+    resource_type: 'auto',
+    format: async (_req: Request, file: { mimetype: string }) => {
+      if (file.mimetype.startsWith('image/')) return 'jpg';
+      if (file.mimetype.startsWith('video/')) return 'mp4';
+      return 'bin';
+    },
+    public_id: (_req: Request, file: Express.Multer.File) => {
+      return `${Date.now()}-${file.originalname}`;
+    },
   } as unknown as CloudinaryParams,
 });
 
