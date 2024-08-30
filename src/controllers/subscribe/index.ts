88889a5c08024de54de7b46e93ba0a64388348ba
techstackmedia <techstackmedia@gmail.com>
2024-08-30 21:03:28 +0000

    feat: send email notification on successful user subscription

--- src/controllers/subscribe/index.ts
+++ src/controllers/subscribe/index.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
 import Subscribe from '../../models/subscribe';
+import User from '../../models/user';
+import { JwtPayload } from 'jsonwebtoken';
+import { sendMail } from '../../config/nodemailer';
+
+function isJwtPayload(user: string | JwtPayload | undefined): user is JwtPayload {
+  return (user as JwtPayload)?.id !== undefined;
+}
 
 const createEmailSubscription = async (req: Request, res: Response) => {
   const { email } = req.body;
@@ -10,6 +17,18 @@ const createEmailSubscription = async (req: Request, res: Response) => {
   try {
     const subscribe = new Subscribe({ email });
     const savedSubscribe = await subscribe.save();
+
+    if (isJwtPayload(req.user)) {
+      const userInfo = await User.findById(req.user?.id);
+      if (userInfo) {
+        const userEmail = userInfo.email;
+        const subject = 'Subscription Confirmation';
+        const text = `<p>Dear Subscriber,</p><p>Thank you for subscribing to our newsletter. We're excited to keep you updated with our latest news and offers.</p><p>Best regards,\nTechStackSpace Shop`;
+
+        await sendMail(userEmail, subject, text);
+      }
+    }
+
     res
       .status(201)
       .json({ message: 'Email subscribed successfully', savedSubscribe });
