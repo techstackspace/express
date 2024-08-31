0d210b9781d8e5f6d80d11d8920e310bee3acef9
techstackmedia <techstackmedia@gmail.com>
2024-08-31 12:21:54 +0000

    feat: define the fields to seach for in each model

---
+++ src/utils/search/fields/index.ts
@@ -0,0 +1,9 @@
+export const modelSearchFields = {
+  Bookmark: ['product.name'],
+  Cart: ['product.name'],
+  Product: ['name', 'description', 'category'],
+  User: ['username', 'email'],
+  Review: ['content'],
+  Comment: ['content'],
+  Order: ['orderNumber'],
+};
