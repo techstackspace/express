e5a7c2578574b5a08f716bb759d5eddb184231ba
techstackmedia <techstackmedia@gmail.com>
2024-08-31 17:18:07 +0000

    feat: add Joi validation to feedback creation controller
    - remove comments

--- src/controllers/feedback/index.ts
+++ src/controllers/feedback/index.ts
@@ -3,6 +3,7 @@ import Feedback from '../../models/feedback';
 import User from '../../models/user';
 import { sendMail } from '../../config/nodemailer';
 import { JwtPayload } from 'jsonwebtoken';
+import { feedbackSchema } from '../../validation/feedback';
 
 function isJwtPayload(
   user: string | JwtPayload | undefined
@@ -11,6 +12,29 @@ function isJwtPayload(
 }
 
 const createFeedback = async (req: Request, res: Response) => {
+  const userAgent = req.headers['user-agent'] || '';
+  let detectedPlatform = 'web';
+
+  if (/mobile/i.test(userAgent)) {
+    detectedPlatform = 'mobile';
+  } else if (/tablet|ipad/i.test(userAgent)) {
+    detectedPlatform = 'tablet';
+  } else if (/macintosh|windows|linux/i.test(userAgent)) {
+    detectedPlatform = 'desktop';
+  }
+
+  const browserInfo = userAgent;
+
+  const { error, value } = feedbackSchema.validate({
+    ...req.body,
+    platform: detectedPlatform,
+    browserInfo,
+  });
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
   const {
     userId,
     productId,
@@ -24,28 +48,7 @@ const createFeedback = async (req: Request, res: Response) => {
     images,
     contactPermission,
     appVersion,
-  } = req.body;
-
-  const userAgent = req.headers['user-agent'] || '';
-
-  let detectedPlatform = 'web';
-
-  if (/mobile/i.test(userAgent)) {
-    detectedPlatform = 'mobile';
-  } else if (/tablet|ipad/i.test(userAgent)) {
-    detectedPlatform = 'tablet';
-  } else if (/macintosh|windows|linux/i.test(userAgent)) {
-    detectedPlatform = 'desktop';
-  }
-
-  const browserInfo = userAgent;
-
-  if (!userId || !productId || !rating || rating < 1 || rating > 5) {
-    return res.status(400).json({
-      error:
-        "Invalid request. The 'userId', 'productId', and 'rating' fields are required, and 'rating' must be a number between 1 and 5.",
-    });
-  }
+  } = value;
 
   try {
     const feedback = new Feedback({
@@ -66,6 +69,7 @@ const createFeedback = async (req: Request, res: Response) => {
     });
 
     const savedFeedback = await feedback.save();
+
     if (isJwtPayload(req.user)) {
       if (contactPermission) {
         const user = await User.findById(req.user?.id);
@@ -74,16 +78,6 @@ const createFeedback = async (req: Request, res: Response) => {
           const subject = 'Thank You for Your Feedback!';
           const text = `<p>Dear ${user?.name},</p><p>Thank you for your feedback on our ${feedbackType}.</p><p>Best regards,<br>TechStackSpace Shop</p>`;
           await sendMail(userEmail, subject, text);
-
-          // const mailOptions = {
-          //   from: '"Your Company" <no-reply@yourcompany.com>',
-          //   to: user.email, // User's email
-          //   subject: 'Thank You for Your Feedback!',
-          //   text: `Dear ${user.name},\n\nThank you for your feedback on our ${feedbackType}.\n\nBest regards,\nYour Company`, // Plain text body
-          //   html: `<p>Dear ${user.name},</p><p>Thank you for your feedback on our ${feedbackType}.</p><p>Best regards,<br>Your Company</p>`, // HTML body
-          // };
-
-          // await transporter.sendMail(mailOptions);
         }
       }
     }
